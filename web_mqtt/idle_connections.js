c132c125a535986f57fe2aa02f4650de74455e7b
Erica Weistrand <34507514+weistrand@users.noreply.github.com>
2023-09-29 11:25:06 +0200

    Use unique client id:s for each connection

--- web_mqtt/idle_connections.js
+++ web_mqtt/idle_connections.js
@@ -1,22 +1,22 @@
-import * as mqtt from "mqtt";
+import * as mqtt from "mqtt"
 import {} from 'dotenv/config'
-import { clientSettings } from './config.js';
+import { clientSettings } from './config.js'
 
 
 const host = clientSettings['host']
 let options = clientSettings['options']
 
 function connection(index) {
-    let clientId = `publisher_${index.toString()}`
+    let clientId = Math.random()
     options['clientId'] = clientId
     const client = mqtt.connect(host, options)
-    
+
     client.on('error', (err) => {
         console.log('CONNECTION ERROR: ', err)
         client.end()
     })
     client.on('connect', () => {
-        console.log(`CLIENT CONNECTED: ${clientId}`)
+        console.log(`CLIENT CONNECTED: ${index}`)
     })
 }
 
@@ -24,10 +24,7 @@ function idleConnections(connections) {
     console.log(`connections: ${connections}`)
     for(let i = 0; i < connections; i++) {
         connection(i)
-    }  
+    }
 }
 
 export default idleConnections
-
-
-
