88ac515913a7a6bea6e08a28902abdf13f8c359f
Erica Weistrand <34507514+weistrand@users.noreply.github.com>
2023-10-08 23:00:34 +0200

    Error handling

--- web_amqp/connections_with_messages.js
+++ web_amqp/connections_with_messages.js
@@ -10,15 +10,19 @@ async function connectionsWithMessages (connections) {
     for (let i = 0; i < connections; i++) {
         const amqp = new AMQPWebSocketClient(clientSettings.host, clientSettings.username, clientSettings.username, clientSettings.password)
         const clientId = Math.floor(Date.now() * Math.random())
-        const conn = await amqp.connect()
-        console.log(`Connection number ${i} established`)
-        const ch = await conn.channel()
-        const q = await ch.queue(`queue_${clientId}`)
-        await q.subscribe({noAck: true, exclusive: true}, onMessage)
+        try {
+            const conn = await amqp.connect()
+            console.log(`Connection number ${i} established`)
+            const ch = await conn.channel()
+            const q = await ch.queue(`queue_${clientId}`)
+            await q.subscribe({noAck: true, exclusive: true}, onMessage)
 
-        setInterval(() => {
-            q.publish(`Message from connection ${i}`, {deliveryMode: 1})
-        }, randomInterval())
+            setInterval(() => {
+                q.publish(`Message from connection ${i}`, {deliveryMode: 1})
+            }, randomInterval())
+        } catch (error) {
+            console.log(`error: ${error}`)
+        }
     }
 }
 
