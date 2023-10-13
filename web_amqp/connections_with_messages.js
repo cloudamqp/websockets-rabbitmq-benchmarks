863b44214bc24ed4bf4c40b70377c8e086923cbd
Erica Weistrand <34507514+weistrand@users.noreply.github.com>
2023-10-13 13:11:36 +0200

    Log error

--- web_amqp/connections_with_messages.js
+++ web_amqp/connections_with_messages.js
@@ -9,15 +9,19 @@ function onMessage (msg) {
 async function connectionsWithMessages (connections) {
     for (let i = 0; i < connections; i++) {
         const amqp = new AMQPWebSocketClient(clientSettings.host, clientSettings.username, clientSettings.username, clientSettings.password)
-        const conn = await amqp.connect()
-        console.log(`Connection number ${i} established`)
-        const ch = await conn.channel()
-        const q = await ch.queue('')
-        await q.subscribe({noAck: true, exclusive: true}, onMessage)
+        try {
+            const conn = await amqp.connect()
+            console.log(`Connection number ${i} established`)
+            const ch = await conn.channel()
+            const q = await ch.queue('')
+            await q.subscribe({noAck: true, exclusive: true}, onMessage)
 
-        setInterval(() => {
-            q.publish(`Message from connection ${i}`, {deliveryMode: 1})
-        }, randomInterval())
+            setInterval(() => {
+                q.publish(`Message from connection ${i}`, {deliveryMode: 1})
+            }, randomInterval())
+        } catch (error) {
+            console.error(error)
+        }
     }
 }
 
